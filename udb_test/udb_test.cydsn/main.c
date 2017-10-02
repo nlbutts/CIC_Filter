b93f107c86016bf3de9c14b53d0497dbace54692
Nicholas Butts <nlbutts@ieee.org>
2017-10-01 20:21:24 -0500

    Updated to actual work.

--- udb_test/udb_test.cydsn/main.c
+++ udb_test/udb_test.cydsn/main.c
@@ -9,51 +9,90 @@
  *
  * ========================================
 */
+#include <stdint.h>
 #include "project.h"
 
 #define ELEMENTS(x)     (sizeof(x)/sizeof(x[0]))
+
+uint16_t adcBuf[2][2000];
+uint8_t currentAdcBuf = 0;
+uint8 adcDMA_Chan;
+uint8 adcDMA_TD[1];
+ 
+void configureDMA(uint16_t * ptr)
+{
+    CyDmaTdSetConfiguration(adcDMA_TD[0], 1000, CY_DMA_DISABLE_TD, CY_DMA_TD_TERMIN_EN | CY_DMA_TD_INC_DST_ADR);
+    CyDmaTdSetAddress(adcDMA_TD[0], LO16((uint32)ADC_SAR_SAR_WRK0_PTR), LO16((uint32)ptr));
+    CyDmaChSetInitialTd(adcDMA_Chan, adcDMA_TD[0]);
+    CyDmaChEnable(adcDMA_Chan, 1);    
+}
+
+CY_ISR(adc_isr)
+{
+    currentAdcBuf = currentAdcBuf ? 0 : 1;
+    configureDMA(adcBuf[currentAdcBuf]);
+}
+
+uint32_t executeCIC(uint16_t *x, int32_t * y, uint32_t len)
+{
+    #define N 4
+    #define R 10
+    static int32_t sigma[N]  = {0};
+    static int32_t delta[N]  = {0};
+    static int32_t pDelta[N] = {0};
+
+    for (uint32_t i = 0; i < len; i++)
+    {
+        pDelta[0] = sigma[3];
+        pDelta[1] = delta[1];
+        pDelta[2] = delta[2];
+        pDelta[3] = delta[3];
+
+        for (uint32_t n = 0; n < R; n++)
+        {
+            sigma[0] += x[(i*R)+n];
+            sigma[1] += sigma[0];
+            sigma[2] += sigma[1];
+            sigma[3] += sigma[2];
+        }
+
+        delta[1] = sigma[3] - pDelta[0];
+        delta[2] = delta[1] - pDelta[1];
+        delta[3] = delta[2] - pDelta[2];
+        y[i] = delta[3] - pDelta[3];
+    }
+    return len;
+}
 
 int main(void)
 {
     CyGlobalIntEnable; /* Enable global interrupts. */
     
-    volatile uint32 d0[100];
-    volatile uint32 a0[100];
-    volatile uint32 a1[100];
-    volatile uint8 status[100];
-    volatile uint32 index = 0;
+    WaveDAC8_Start();
+    ADC_SAR_Start();
+    Timer_Start();
     
-    memset((void*)d0, 0, sizeof(d0));
-    memset((void*)a0, 0, sizeof(a0));
-    memset((void*)a1, 0, sizeof(a1));
-    memset((void*)status, 0, sizeof(status));
-    
-    Control_Reg_Write(0);    
-    d0[0] = cic_1_GetD0();
-    a0[0] = cic_1_GetA0();
-    a1[0] = cic_1_GetA1();
-    status[0] = Status_Reg_Read();
+    /* Defines for adcDMA */
+    #define adcDMA_BYTES_PER_BURST 2
+    #define adcDMA_REQUEST_PER_BURST 1
+    #define adcDMA_SRC_BASE (CYDEV_PERIPH_BASE)
+    #define adcDMA_DST_BASE (CYDEV_SRAM_BASE)
 
-    cic_1_Start();
+    /* DMA Configuration for adcDMA */
+    adcDMA_Chan = adcDMA_DmaInitialize(adcDMA_BYTES_PER_BURST, adcDMA_REQUEST_PER_BURST, 
+        HI16(adcDMA_SRC_BASE), HI16(adcDMA_DST_BASE));
+    adcDMA_TD[0] = CyDmaTdAllocate();
+    configureDMA(adcBuf[0]);
     
-    Control_Reg_Write(1); 
-    for(;;)
-    {
-        if (index < 100)
-        {
-            d0[index] = cic_1_GetD0();
-            a0[index] = cic_1_GetA0();
-            a1[index] = cic_1_GetA1();
-            status[index] = Status_Reg_Read();
-            index++;
-        }
-        else
-        {
-            index = 1000;
-        }
-        
-    }
+    adcISR_StartEx(adc_isr);
     
+    ADC_SAR_StartConvert();     
+    
+    int32_t y[100];
+    volatile uint16_t start = Timer_ReadCounter();
+    executeCIC(adcBuf[0], y, 100);
+    volatile uint16_t stop = Timer_ReadCounter();
+    volatile uint16_t diff = stop - start;
     
     for(;;)
     {
